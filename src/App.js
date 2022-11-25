45d39fa88d9bd90ec5aa4c337d70b7dbf2d21ab3
Valentoine <vava78@live.fr>
2022-11-25 23:50:58 +0100

    Make index part 1

--- src/App.js
+++ src/App.js
@@ -17,14 +17,14 @@ function App() {
         </header>
 
         <main>
-          <section class="py-5 text-center container">
+          <section id="begin_bkg" class="py-4 text-center">
             <div class="row py-lg-5">
               <div class="col-lg-6 col-md-8 mx-auto">
                 <h1 class="fw-light">Bienvenue utilisatrique</h1>
-                <p class="lead text-muted">Tu as rejoint cette aventure. </p>
+                <p class="lead text-black-50">Tu as rejoint cette aventure. </p>
                 <p>
-                  <a href="#" class="btn btn-primary my-2">Boutton 1</a>
-                  <a href="#" class="btn btn-secondary my-2">Boutton 2</a>
+                  <a href="" class="btn btn-primary my-2 mx-2">J'aime la saucisse</a>
+                  <a href="" class="btn btn-secondary my-2 mx-2">T'es con Jérôme</a>
                 </p>
               </div>
             </div>
@@ -37,8 +37,8 @@ function App() {
 
                 <div class="col">
                   <div class="card shadow-sm">
-                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: صورة مصغرة" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Chuut</text>
-                    </svg>
+                    <img src="https://media.tenor.com/uEoE0W4-UEoAAAAM/confused-lady-math-lady.gif" class="card-img-top card_image bd-placeholder-img" alt="..."></img>
+                    <div class="center fs-5 text-white fw-semibold">Enigme 1</div>
                     <div class="card-body">
                       <p class="card-text">Ceci est la phrase lol</p>
                       <div class="d-flex justify-content-between align-items-center">
@@ -53,8 +53,8 @@ function App() {
                 </div>
                 <div class="col">
                   <div class="card shadow-sm">
-                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: صورة مصغرة" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Chuut</text>
-                    </svg>
+                    <img src="http://img0.joyreactor.com/pics/post/erotic-boobs-lesbian-gif-1130938.gif" class="card-img-top card_image bd-placeholder-img" alt="..."></img>
+                    <div class="center fs-5 text-white fw-semibold">Enigme 2</div>
                     <div class="card-body">
                       <p class="card-text">Ceci est la phrase lol</p>
                       <div class="d-flex justify-content-between align-items-center">
@@ -69,8 +69,106 @@ function App() {
                 </div>
                 <div class="col">
                   <div class="card shadow-sm">
-                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: صورة مصغرة" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Chuut</text>
-                    </svg>
+                  <img src="https://media.tenor.com/7Hz6nvBBC3QAAAAC/cats-chess.gif" class="card-img-top card_image bd-placeholder-img" alt="..."></img>
+                  <div class="center fs-5 text-white fw-semibold">Enigme 3</div>
+                    <div class="card-body">
+                      <p class="card-text">Ceci est la phrase lol</p>
+                      <div class="d-flex justify-content-between align-items-center">
+                        <div class="btn-group">
+                          <button type="button" class="btn btn-sm btn-outline-secondary">Boutton 1</button>
+                          <button type="button" class="btn btn-sm btn-outline-secondary">Boutton 2</button>
+                        </div>
+                        <small class="text-muted">Anecdote</small>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col">
+                  <div class="card shadow-sm">
+                    <img src="https://i.imgur.com/Ub3U01z.gif" class="card-img-top card_image bd-placeholder-img" alt="..."></img>
+                    <div class="center fs-5 text-white fw-semibold">Enigme 4</div>
+                    <div class="card-body">
+                      <p class="card-text">Ceci est la phrase lol</p>
+                      <div class="d-flex justify-content-between align-items-center">
+                        <div class="btn-group">
+                          <button type="button" class="btn btn-sm btn-outline-secondary">Boutton 1</button>
+                          <button type="button" class="btn btn-sm btn-outline-secondary">Boutton 2</button>
+                        </div>
+                        <small class="text-muted">Anecdote</small>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="card shadow-sm">
+                    <img src="https://media4.giphy.com/media/xghFgmOKbk0G4/200w.webp?cid=ecf05e47k9xzioh1hx6754bros9qg3rcnma5r9vc47hjuw92&rid=200w.webp&ct=g" class="card-img-top card_image bd-placeholder-img" alt="..."></img>
+                    <div class="center fs-5 text-white fw-semibold">Enigme 5</div>
+                    <div class="card-body">
+                      <p class="card-text">Ceci est la phrase lol</p>
+                      <div class="d-flex justify-content-between align-items-center">
+                        <div class="btn-group">
+                          <button type="button" class="btn btn-sm btn-outline-secondary">Boutton 1</button>
+                          <button type="button" class="btn btn-sm btn-outline-secondary">Boutton 2</button>
+                        </div>
+                        <small class="text-muted">Anecdote</small>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="card shadow-sm">                    
+                    <img src="https://media3.giphy.com/media/llZVEOIi9tCVxFskpY/giphy.gif?cid=ecf05e47nw19xgosyve3zpw4wq4z1qxikuweisq8xzhc1ezd&rid=giphy.gif&ct=g" class="card-img-top card_image bd-placeholder-img" alt="..."></img>
+                    <div class="center fs-5 text-white fw-semibold">Enigme 6</div>
+                    <div class="card-body">
+                      <p class="card-text">Ceci est la phrase lol</p>
+                      <div class="d-flex justify-content-between align-items-center">
+                        <div class="btn-group">
+                          <button type="button" class="btn btn-sm btn-outline-secondary">Boutton 1</button>
+                          <button type="button" class="btn btn-sm btn-outline-secondary">Boutton 2</button>
+                        </div>
+                        <small class="text-muted">Anecdote</small>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col">
+                  <div class="card shadow-sm">
+                    <img src="https://media0.giphy.com/media/l2JedjCkIJxQvRZmM/200.webp?cid=ecf05e47lyqvs2w21xm4daz0an16c91za4pb841k91944zch&rid=200.webp&ct=g" class="card-img-top card_image bd-placeholder-img" alt="..."></img>
+                    <div class="center fs-5 text-white fw-semibold">Enigme 7</div>
+                    <div class="card-body">
+                      <p class="card-text">Ceci est la phrase lol</p>
+                      <div class="d-flex justify-content-between align-items-center">
+                        <div class="btn-group">
+                          <button type="button" class="btn btn-sm btn-outline-secondary">Boutton 1</button>
+                          <button type="button" class="btn btn-sm btn-outline-secondary">Boutton 2</button>
+                        </div>
+                        <small class="text-muted">Anecdote</small>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="card shadow-sm">
+                    <img src="https://media0.giphy.com/media/ibGFpMv1Uoais/200.webp?cid=ecf05e47vrjvjccq8szfdlpu9rjkqru8olte26kdm5zpo4zj&rid=200.webp&ct=g" class="card-img-top card_image bd-placeholder-img" alt="..."></img>
+                    <div class="center fs-5 text-white fw-semibold">Enigme 8</div>
+                    <div class="card-body">
+                      <p class="card-text">Ceci est la phrase lol</p>
+                      <div class="d-flex justify-content-between align-items-center">
+                        <div class="btn-group">
+                          <button type="button" class="btn btn-sm btn-outline-secondary">Boutton 1</button>
+                          <button type="button" class="btn btn-sm btn-outline-secondary">Boutton 2</button>
+                        </div>
+                        <small class="text-muted">Anecdote</small>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="card shadow-sm">
+                    <img src="https://media0.giphy.com/media/zrxazUScjhxo4/200w.webp?cid=ecf05e47of5uql7n2u1t00o7hrz6khxl9dx3avni1vxrugqw&rid=200w.webp&ct=g" class="card-img-top card_image bd-placeholder-img" alt="..."></img>
+                    <div class="center fs-5 text-white fw-semibold">Enigme 8</div>
                     <div class="card-body">
                       <p class="card-text">Ceci est la phrase lol</p>
                       <div class="d-flex justify-content-between align-items-center">
@@ -96,7 +194,6 @@ function App() {
             <p class="mb-1">Fait par société à capital</p>
           </div>
         </footer>
-
       </body>
     </div>
     );
